80d6d16f4be3237985711b6ce05dfbbb040b4743
Anmol Kansal <41195162+kansalanmol0609@users.noreply.github.com>
2019-07-05 17:49:54 +0530

    Fixed a data type error
    Transaction Amount, Customer ID and Order ID may not be string and thus it returns the following error while performing a transaction: -
    TypeError: params[key].includes is not a function.
    I added an 'if' block outside beforw using ".includes" function and it resolved the issue.

--- checksum/checksum.js
+++ checksum/checksum.js
@@ -11,16 +11,18 @@ function paramsToString(params, mandatoryflag) {
   var tempKeys = Object.keys(params);
   tempKeys.sort();
   tempKeys.forEach(function (key) {
-  var n = params[key].includes("REFUND"); 
-   var m = params[key].includes("|");  
-        if(n == true )
-        {
-          params[key] = "";
-        }
-          if(m == true)
-        {
-          params[key] = "";
-        }  
+    if (key !== 'ORDER_ID' && key !== 'CUST_ID' && key !== 'TXN_AMOUNT') {  
+      var n = params[key].includes("REFUND"); 
+      var m = params[key].includes("|");  
+          if(n == true )
+          {
+            params[key] = "";
+          }
+            if(m == true)
+          {
+            params[key] = "";
+          }  
+    }
     if (key !== 'CHECKSUMHASH' ) {
       if (params[key] === 'null') params[key] = '';
       if (!mandatoryflag || mandatoryParams.indexOf(key) !== -1) {
